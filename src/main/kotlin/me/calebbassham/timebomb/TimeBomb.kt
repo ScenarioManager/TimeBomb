bd8e43f5fb88df10cdbf230a149a4b1cae02d86a
Caleb Bassham <caleb.bassham@gmail.com>
2018-09-15 18:19:03 -0500

    Fix chest placement

--- src/main/kotlin/me/calebbassham/timebomb/TimeBomb.kt
+++ src/main/kotlin/me/calebbassham/timebomb/TimeBomb.kt
@@ -4,11 +4,12 @@ import me.calebbassham.scenariomanager.api.Scenario
 import me.calebbassham.scenariomanager.api.ScenarioEvent
 import me.calebbassham.scenariomanager.plugin.ScenarioManagerPlugin
 import org.bukkit.Material
+import org.bukkit.block.BlockFace
+import org.bukkit.block.Chest
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
-import org.bukkit.inventory.Inventory
 import org.bukkit.plugin.java.JavaPlugin
 
 class TimeBomb(plugin: JavaPlugin): Scenario("TimeBomb", plugin), Listener {
@@ -17,23 +18,43 @@ class TimeBomb(plugin: JavaPlugin): Scenario("TimeBomb", plugin), Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST) // Any drops added in other death events will be added to chest
     fun onPlayerDeath(e: PlayerDeathEvent) {
-        val drops = e.drops
+        val drops = e.drops.filter { it != null }.filter { it.type != Material.AIR }.toTypedArray()
+        e.drops.clear()
 
-        val chest1 = e.entity.location.add(0.0, 1.0, 0.0)
-        chest1.block.type = Material.CHEST
+        val chest1 = e.entity.location.block
+        chest1.type = Material.CHEST
+
+        val chest1data = chest1.blockData as org.bukkit.block.data.type.Chest
+        chest1data.facing = BlockFace.SOUTH
+
+        chest1.blockData = chest1data
 
         if(drops.size > 27) {
-            val chest2 = chest1.add(1.0, 0.0, 0.0)
-            chest2.block.type = Material.CHEST
+
+
+            chest1data.type = org.bukkit.block.data.type.Chest.Type.RIGHT
+
+
+            chest1.getRelative(BlockFace.EAST).apply {
+                val chest2 = this
+                chest2.type = Material.CHEST
+
+                val chest2data = chest2.blockData as org.bukkit.block.data.type.Chest
+                chest2data.facing = BlockFace.SOUTH
+
+                chest2data.type = org.bukkit.block.data.type.Chest.Type.LEFT
+
+                chest2.blockData = chest2data
+            }
         }
 
-        val chestInv = chest1.block as Inventory
+        val chest = chest1.state as Chest
 
-        chestInv.contents = drops.toTypedArray()
+        chest.inventory.contents = drops
 
         ScenarioManagerPlugin.scenarioManager?.eventScheduler?.scheduleEvent(object : ScenarioEvent("${e.entity.displayName} explodes", true) {
             override fun run() {
-                chest1.world.createExplosion(chest1, 4f)
+                chest1.world.createExplosion(chest1.location, 4f)
             }
         }, 20 * 30)
     }
